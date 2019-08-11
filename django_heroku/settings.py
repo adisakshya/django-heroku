29108b88605315268e1bb5c6e0eba90ad963a1a0
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-08-11 22:59:18 +0530

    Fix: middleware for whitenoise

--- django_heroku/settings.py
+++ django_heroku/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_heroku.urls'

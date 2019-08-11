12e2b0381866007c660ef810bd0c5e601f699a67
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-08-11 22:27:17 +0530

    updated allowed_hosts and DEBUG mode

--- django_heroku/django_heroku/settings.py
+++ django_heroku/django_heroku/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%a-zw83z$r20gjn&$oebp77op6*7=_-&yds^!o@k(us*_t-8&8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
